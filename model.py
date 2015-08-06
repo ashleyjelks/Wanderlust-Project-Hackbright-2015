f449212011ac536e7d18ef57290fdcf0a2282849
Ashley Jelks <arjelks@gmail.com>
2015-08-05 17:51:14 -0700

    completed model and database, server up and running

--- model.py
+++ model.py
@@ -27,9 +27,8 @@ class User(db.Model):
 
 
 class Flight(db.Model):
-    #Flight is a subclass of db.Model
 
-    """ Flight database info """
+    """ Flight database info for travelers"""
 
     __tablename__ = "flights"
 
@@ -48,7 +47,7 @@ class Flight(db.Model):
     flight_duration = db.Column(db.Integer, nullable=True)
     airline = db.Column(db.String(75), nullable=True)
     flight_number = db.Column(db.String(10), nullable=False)
-    base_fare =  db.Column(db.String(10), nullable=False)
+    base_fare = db.Column(db.String(10), nullable=False)
     taxes = db.Column(db.String(10), nullable=False)
     total_fare = db.Column(db.String(10), nullable=False)
     destination_region = db.Column(db.String, nullable=True)
@@ -68,95 +67,66 @@ class Flight(db.Model):
     third_connection_date_time_departs = db.Column(db.String(22), nullable=True)
     third_connection_date_time_arrives = db.Column(db.String(22), nullable=True)
 
-    
-    
-
     """Currently are returning the date/time data from the API call as a string, however
-    you might need to use a datetime formatter here (db.datetime instead of db.String) 
+    you might need to use a datetime formatter here (db.datetime instead of db.String)
     when you seed your data .seed.py Something to consider!"""
 
-    # time_departing_flight_departs = db.Column(db.String(22), nullable=False) 
-    # time_departing_flight_arrives = db.Column(db.String(22), nullable=False) 
-    # date_departing_flight_departs = db.Column(db.String(22), nullable=False) 
-    # date_departing_flight_arives = db.Column(db.String(22), nullable=False) 
-    # time_return_flight_departs = db.Column(db.String(22), nullable=False) 
-    # time_departing_flight_arrives = db.Column(db.String(22), nullable=False) 
-    # date_return_flight_departs = db.Column(db.String(22), nullable=False) 
+    # time_departing_flight_departs = db.Column(db.String(22), nullable=False)
+    # time_departing_flight_arrives = db.Column(db.String(22), nullable=False)
+    # date_departing_flight_departs = db.Column(db.String(22), nullable=False)
+    # date_departing_flight_arives = db.Column(db.String(22), nullable=False)
+    # time_return_flight_departs = db.Column(db.String(22), nullable=False)
+    # time_departing_flight_arrives = db.Column(db.String(22), nullable=False)
+    # date_return_flight_departs = db.Column(db.String(22), nullable=False)
     # date_return_flight_arives = db.Column(db.String(22), nullable=False)
 
 
-
-class Save(db.Model):
+class SavedSearch(db.Model):
 #Save is a subclass of db.Model
 
     """Database of saved user searches in Wanderlust Application"""
 
-    __tablename__ = "saves"
+    __tablename__ = "saved_searches"
 
     search_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
     search_info = db.Column(db.String(1000), nullable=True)
     user_id = db.Column(db.Integer, nullable=False)
 
 
-class Flight(db.Model):
-    #Flight is a subclass of db.Model
+class FlightUserMap(db.Model):
 
-    """ Flight database info """
+    """ The association table for the many-to-many associations between users and flight tables"""
 
-    __tablename__ = "flights"
+    __tablename__ = "flight_user_maps"
 
-    flight_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
-    origin_city_name = db.Column(db.String(75), nullable=False)
-    origin_airport_name = db.Column(db.String(200), nullable=False)
-    origin_airport_code = db.Column(db.String(3), nullable=False)
-    origin_flight_duration = db.Column(db.Integer, nullable=True)
-    destination_city_name = db.Column(db.String(75), nullable=False)
-    destination_airport_name = db.Column(db.String(200), nullable=False)
-    destination_airport_code = db.Column(db.String(3), nullable=False)
-    date_time_departing_flight_arrives = db.Column(db.String(22), nullable=False)
-    date_time_departing_flight_departs = db.Column(db.String(22), nullable=False)
-    date_time_return_flight_arrives = db.Column(db.String(22), nullable=False)
-    date_time_return_flight_departs = db.Column(db.String(22), nullable=False)
-    flight_duration = db.Column(db.Integer, nullable=True)
-    airline = db.Column(db.String(75), nullable=True)
-    flight_number = db.Column(db.String(10), nullable=False)
-    base_fare =  db.Column(db.String(10), nullable=False)
-    taxes = db.Column(db.String(10), nullable=False)
-    total_fare = db.Column(db.String(10), nullable=False)
-    destination_region = db.Column(db.String, nullable=True)
-    first_connection_city_name = db.Column(db.String(75), nullable=True)
-    first_connection_airport_name = db.Column(db.String(200), nullable=True)
-    first_connection_airport_code = db.Column(db.String(3), nullable=True)
-    first_connection_date_time_departs = db.Column(db.String(22), nullable=True)
-    first_connection_date_time_arrives = db.Column(db.String(22), nullable=True)
-    second_connection_city_name = db.Column(db.String(75), nullable=True)
-    second_connection_airport_name = db.Column(db.String(200), nullable=True)
-    second_connection_airport_code = db.Column(db.String(3), nullable=True)
-    second_connection_date_time_departs = db.Column(db.String(22), nullable=True)
-    second_connection_date_time_arrives = db.Column(db.String(22), nullable=True)
-    third_connection_city_name = db.Column(db.String(75), nullable=True)
-    third_connection_airport_name = db.Column(db.String(200), nullable=True)
-    third_connection_airport_code = db.Column(db.String(3), nullable=True)
-    third_connection_date_time_departs = db.Column(db.String(22), nullable=True)
-    third_connection_date_time_arrives = db.Column(db.String(22), nullable=True)
-
-
-class Mapping(db.Model):
-
-    """ The association table for the many-to-many associations between users and flights tables"""
-
-
-    __tablename__ = "mappings"
-
-    association_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
+    mapping_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
     flight_id = db.Column(db.Integer, db.ForeignKey('flights.flight_id'), nullable=False)
 
+    #Defines a relationship to a user
+    user = db.relationship("User", backref=db.backref("flight_user_maps", order_by=mapping_id))
 
-    #Defines a relationship to a flight search
+    flight = db.relationship("Flight", backref=db.backref("flight_user_maps", order_by=mapping_id))
 
 
+##############################################################################
+# Helper functions
 
 
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
 
-# INCLUDE REALATIONSHIP TABLES AND BACKREFERENCES    
+    # Configure to use our SQLite database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flights.db'
+    db.app = app
+    db.init_app(app)
+    db.app.config["SQLALCHEMY_ECHO"] = True
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print "Connected to DB."
---
+++ server.py
@@ -0,0 +1,48 @@
+"""Wanderlust App Server"""
+
+from jinja2 import StrictUndefined
+
+from flask import Flask, render_template, redirect, request, flash, session
+
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import User, Flight, FlightUserMap, SavedSearch, connect_to_db, db
+
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+""" FIGURE OUT HOW TO HIDE THE APP secret_key ON GITHUB  """
+
+# Normally, if you use an undefined variable in Jinja2, it fails silently.
+# This is horrible. Fix this so that, instead, it raises an error.
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+    """Homepage."""
+
+    return render_template("index.html")
+
+
+"""
+YOU NEED TO CREATE AN INSTANCE OF THE FLIGHT CLASS TO INCLUDE FLIGHT DATA FOR THE 2ND TRAVELER,
+USE THIS CODE SNIPPET FROM SEED.PY FROM RATINGS
+new_user = User(user_id=new_user_id, age=user_age, zipcode=user_zipcode)
+#new_user is the object. new_user is also an instance of the User class.
+"""
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the point
+    # that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run()
