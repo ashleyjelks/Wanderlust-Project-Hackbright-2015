df9314b6a3ee6b0779a37543482912f1b78d872c
Ashley Jelks <arjelks@gmail.com>
2015-08-17 18:08:12 -0700

    new updates

--- model.py
+++ model.py
@@ -10,39 +10,37 @@ db = SQLAlchemy()
 
 # Defining all Models in this database
 
-# class CodeRegion(db.Model):
-
-#     """ Aiport codes by region"""
-
-#     __tablename__ = "code_regions"
-
-#     region_id = db.Column(db.Integer, autoincrement=True, nullable=False)
-#     code = db.Column(db.String(3), primary_key=True, nullable=False)
-#     region = db.Column(db.String(35), nullable=False)
-
-
 class User(db.Model):
     #User is a sublclass of db.Model
 
-    """ User of Wanderlust Application """
+    """ User of Wanderlust Application and their search parameters."""
 
     __tablename__ = "users"
 
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
-    name = db.Column(db.String(150), nullable=False)
-    username = db.Column(db.String(150), nullable=True)
-    email = db.Column(db.String(150), nullable=False)
-    login = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(150), nullable=True)
+    email = db.Column(db.String(150), nullable=True)
+    password = db.Column(db.String(100), nullable=True)
+    traveler1_name = db.Column(db.String(150), nullable=True)
+    traveler2_name = db.Column(db.String(150), nullable=True)
+    first_origin = db.Column(db.String(75), nullable=True)
+    second_origin = db.Column(db.String(75), nullable=True)
+    first_max_price = db.Column(db.Integer, nullable=True)
+    second_max_price = db.Column(db.Integer, nullable=True)
+    departure_date = db.Column(db.String(75), nullable=True)
+    return_date = db.Column(db.String(75), nullable=True)
+    destination = db.Column(db.String(75), nullable=True)
+
 
 
 class Flight(db.Model):
 
-    """ Flight database info for travelers"""
+    """ Flight database info for travelers."""
 
     __tablename__ = "flights"
 
     flight_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=True)
     outbound_city_origin = db.Column(db.String(75), nullable=False)
     outbound_city_final_destination = db.Column(db.String(75), nullable=False)
     outbound_airline = db.Column(db.String(75), nullable=False)
@@ -60,161 +58,7 @@ class Flight(db.Model):
     total_fare = db.Column(db.Integer, nullable=False)
     outbound_connecting_city = db.Column(db.String(75), nullable=True)
     inbound_connecting_city = db.Column(db.String(75), nullable=True)
-
-
-class SavedSearch(db.Model):
-#SavedSearch is a subclass of db.Model
-
-    """Database of saved user searches in Wanderlust Application"""
-
-    __tablename__ = "saved_searches"
-
-    search_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
-    search_info = db.Column(db.String(1000), nullable=True)
-    user_id = db.Column(db.Integer, nullable=False)
-
-
-class FlightUserMap(db.Model):
-
-    """ The association table for the many-to-many associations between users and flight tables"""
-
-    __tablename__ = "flight_user_maps"
-
-    mapping_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    flight_id = db.Column(db.Integer, db.ForeignKey('flights.flight_id'), nullable=False)
-
-    #Defines a relationship to a user
-    user = db.relationship("User", backref=db.backref("flight_user_maps", order_by=mapping_id))
-
-    flight = db.relationship("Flight", backref=db.backref("flight_user_maps", order_by=mapping_id))
-
-#############################################################################
-# Helper functions
-
-
-def connect_to_db(app):
-    """Connect the database to our Flask app."""
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flights.db'
-    db.app = app
-    db.init_app(app)
-    db.app.config["SQLALCHEMY_ECHO"] = True
-
-
-if __name__ == "__main__":
-    from server import app
-    connect_to_db(app)
-    print "Connected to DB."
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#     """ Model and database functions for Wanderlust project. """
-
-# from flask_sqlalchemy import SQLAlchemy
-
-# from config.sql import get_sql_config
-# # get_sql_config knows if its testing, development, integration, production, so it nows which settings give you
-
-# # example of sql config
-
-# sql_config = {
-#     'port': 8787,
-#     'host': localhost,
-#     'db': flights.db
-# }
-
-# sql_config = get_sql_config()
-# db = SQLAlchemy(port=sql_config.get('port'), host=sql_config.get('host'), db=sql_config.get('db'))
-
-# # RuntimeError: application not registered on db instance and no application bound to current context = ERROR THAT IS ONE THE PROBLEMS...WHY WON'T IT PASS THE 
-
-# #################################################################
-
-# # Defining all Models in this database
-
-# class CodeRegion(db.Model):
-
-#     """ Aiport codes by region"""
-
-#     __tablename__ = "code_regions"
-
-#     region_id = db.Column(db.Integer, autoincrement=True, nullable=False)
-#     code = db.Column(db.String(3), primary_key=True, nullable=False)
-#     region = db.Column(db.String(35), nullable=False)
-
-
-# class User(db.Model):
-#     #User is a sublclass of db.Model
-
-#     """ User of Wanderlust Application """
-
-#     __tablename__ = "users"
-
-#     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
-#     name = db.Column(db.String(150), nullable=False)
-#     username = db.Column(db.String(150), nullable=True)
-#     email = db.Column(db.String(150), nullable=False)
-#     login = db.Column(db.String(100), nullable=False)
-#     password = db.Column(db.String(100), nullable=False)
-
-
-# class FlightLeg(db.Model):
-
-#     """ Flight database info for travelers"""
-
-#     __tablename__ = "flight_legs"
-
-#     leg_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
-#     city_origin = db.Column(db.String(75), nullable=False)
-#     city_destination = db.Column(db.String(75), nullable=True)
-#     airline = db.Column(db.String(75), nullable=False)
-#     flight_number = db.Column(db.Integer, nullable=False)
-#     datetime_departure = db.Column(db.DateTime, nullable=False)
-#     datetime_arrival = db.Column(db.DateTime, nullable=False)
-
-
-# class Flight(db.Model):
-
-#     """ Flight database info for travelers"""
-
-#     __tablename__ = "flights"
-
-#     flight_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
-#     outbound_flight_leg_id = db.Column(db.Integer, db.ForeignKey('flight_legs.leg_id'))
-#     outbound_connecting_flight_leg_id = db.Column(db.Integer, db.ForeignKey('flight_legs.leg_id'), nullable=True)
-#     inbound_flight_leg_id = db.Column(db.Integer, db.ForeignKey('flight_legs.leg_id'))
-#     inbound_connecting_flight_leg_id = db.Column(db.Integer, db.ForeignKey('flight_legs.leg_id'), nullable=True)
-#     base_fare = db.Column(db.Integer, nullable=False)
-#     taxes = db.Column(db.Integer, nullable=False)
-#     total_fare = db.Column(db.Integer, nullable=False)
-
-#     outbound_leg = db.relationship('FlightLeg',
-#                                    backref=('outbound_legs'),
-#                                    foreign_keys=['Flight.outbound_flight_leg_id'])
-
-#     outbound_connection_leg = db.relationship('FlightLeg',
-#                                               backref=('outbound_connection_legs'),
-#                                               foreign_keys=['Flight.outbound_connecting_flight_leg_id'])
-
-#     inbound_leg = db.relationship('FlightLeg',
-#                                   backref=('inbound_legs'),
-#                                   foreign_keys=['Flight.inbound_flight_leg_id'])
-
-#     inbound_connection_leg = db.relationship('FlightLeg',
-#                                              backref=('inbound_connection_legs'),
-#                                              foreign_keys=['Flight.inbound_connecting_flight_leg_id'])
+    user = db.relationship('User', backref=db.backref('flights', order_by=user_id))
 
 
 # class SavedSearch(db.Model):
@@ -225,8 +69,8 @@ if __name__ == "__main__":
 #     __tablename__ = "saved_searches"
 
 #     search_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
-#     search_info = db.Column(db.String(1000), nullable=True)
-#     user_id = db.Column(db.Integer, nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    
 
 
 # class FlightUserMap(db.Model):
@@ -236,34 +80,35 @@ if __name__ == "__main__":
 #     __tablename__ = "flight_user_maps"
 
 #     mapping_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
-#     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-#     flight_id = db.Column(db.Integer, db.ForeignKey('flights.flight_id'), nullable=False)
-
-#     #Defines a relationship to a user
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=True)
+#     flight_id = db.Column(db.Integer, db.ForeignKey('flights.flight_id'), nullable=True)
+#     # search_id = db.Column(db.Integer, db.ForeignKey('saved_searches.search_id'), nullable=True)
 #     user = db.relationship("User", backref=db.backref("flight_user_maps", order_by=mapping_id))
-
 #     flight = db.relationship("Flight", backref=db.backref("flight_user_maps", order_by=mapping_id))
+#     # search = db.relationship("SavedSearch", backref=("flight_user_maps"), order_by=mapping_id)
 
-
-# # #############################################################################
+#############################################################################
 # Helper functions
 
 
-# def connect_to_db(app):
-#     """Connect the database to our Flask app."""
 
-#     # Configure to use our SQLite database
-#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flights.db'
-#     db.app = app
-#     db.init_app(app)
-#     db.app.config["SQLALCHEMY_ECHO"] = True
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our SQLite database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flights.db'
+    db.app = app
+    db.init_app(app)
 
 
-# if __name__ == "__main__":
-#     # As a convenience, if we run this module interactively, it will leave
-#     # you in a state of being able to work with the database directly.
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
 
-#     from server import app
-#     connect_to_db(app)
-#     print "Connected to DB."
+    from server import app
+    connect_to_db(app)
+    print "Connected to DB."
+
+
+
 
--- server.py
+++ server.py
@@ -1,13 +1,10 @@
-# app.py
-
 """Wanderlust App Server"""
 
 from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-# from model import connect_to_db, db, User, FlightLeg, Flight, SavedSearch, FlightUserMap
-
-# User, Flight, FlightUserMap, SavedSearch,
+from model import connect_to_db, db, User, Flight
+import api_seed as api_seed
 
 
 app = Flask(__name__)
@@ -20,17 +17,57 @@ app.secret_key = "ABC"
 app.jinja_env.undefined = StrictUndefined
 
 
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    """Homepage."""
+
+    """Route to gather a user's search parameters"""
 
     return render_template("index.html")
 
 
+@app.route('/search_results',  methods=['POST'])
+def get_search():
+    """Wanderlust Homepage. Here user inserts search parameters"""
+
+    name = request.form["name"]
+    email = request.form["email"]
+    password = request.form["password"]
+    traveler1_name = request.form["traveler1_name"]
+    traveler2_name = request.form["traveler2_name"]
+    first_origin = request.form["first_origin"]
+    second_origin = request.form["second_origin"]
+    first_max_price = int(request.form["first_max_price"])
+    second_max_price = int(request.form["second_max_price"])
+    departure_date = request.form["departure_date"]
+    return_date = request.form["return_date"]
+    destination = request.form["destination"]
+    print name
+    print email
+    print password
+    print traveler1_name
+    print traveler2_name
+    print first_origin
+    print second_origin
+    print first_max_price
+    print second_max_price
+    print departure_date
+    print return_date
+
+    search_request = User(name=name, email=email, password=password, traveler1_name=traveler1_name, traveler2_name=traveler2_name, first_origin=first_origin, second_origin=second_origin, first_max_price=first_max_price, second_max_price=second_max_price, departure_date=departure_date, return_date=return_date, destination=destination)
+    print search_request
+
+    db.session.add(search_request)
+    db.session.commit()
+
+    t1 = api_seed.get_price_traveler_one(first_origin, destination, departure_date, return_date, first_max_price)
+    t2 = api_seed.get_price_traveler_two(second_origin, destination, departure_date, return_date, second_max_price)
+
+    return render_template("search_results.html", t1=t1, t2=t2)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.debug = True
     connect_to_db(app)
     DebugToolbarExtension(app)
     app.run()
+
